d39afa1ff6dbc1da7c5332b5848d0e29197270af
SnipyJulmy <sylvain.julmy@gmail.com>
2017-11-23 14:36:48 +0100

    Add some custom exception to SkalaLog

---
+++ src/main/scala/skalalog/SkalaLogExceptions.scala
@@ -0,0 +1,10 @@
+package skalalog
+
+
+class ParserException(private val message: String = "",
+                      private val cause: Throwable = None.orNull
+                     ) extends Exception(message, cause)
+
+class NoListFoundException(private val message: String = "",
+                           private val cause: Throwable = None.orNull
+                          ) extends ParserException
