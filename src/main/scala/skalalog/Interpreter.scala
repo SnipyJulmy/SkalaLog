d92fb58bb029e2185f002711b9895c28d75730f3
SnipyJulmy <sylvain.julmy@gmail.com>
2017-11-29 16:55:16 +0100

    Add the basic object for the future skalalog interpreter

---
+++ src/main/scala/skalalog/Interpreter.scala
@@ -0,0 +1,10 @@
+package skalalog
+
+// Interpreter of SkaLog
+// Use to "stop" the search of solution
+// this is (or would be) a standard prolog interpreter to ask questions
+// via the command line interface
+// TODO
+object Interpreter {
+
+}
