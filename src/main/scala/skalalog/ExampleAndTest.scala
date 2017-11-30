0bc881aff700fe08472e3dab3a223cdba6f4061f
SnipyJulmy <sylvain.julmy@gmail.com>
2017-11-30 14:47:45 +0100

    Example is working !

--- src/main/scala/skalalog/ExampleAndTest.scala
+++ src/main/scala/skalalog/ExampleAndTest.scala
@@ -1,7 +1,7 @@
 package skalalog
 
-import Unification._
 import parser._
+import skalalog.Unification._
 
 import scala.util.parsing.input.CharSequenceReader
 
@@ -21,51 +21,17 @@ object ExampleAndTest extends App {
       |father(A) :- parent(A,_),male(A).
     """.stripMargin
 
-  val database = parser.parse(program, new PackratReader[Char](new CharSequenceReader(s))) match {
+  val queryString = "father(A)."
+
+  implicit val database: Program = parser.parse(program, new PackratReader[Char](new CharSequenceReader(s))) match {
     case Success(result, _) => result
     case error: NoSuccess => throw new ParserException(error.msg)
   }
 
-  val q = parser.parse(query, new PackratReader[Char](new CharSequenceReader("father(bob)."))) match {
+  val q = parser.parse(query, new PackratReader[Char](new CharSequenceReader(queryString))) match {
     case Success(result, next) => result
     case error: NoSuccess => throw new ParserException(error.msg)
   }
 
-  println(ask(q, database))
-
-  def ask(query: Query, database: Program): Option[Theta] = {
-    // TODO process query additional clauses
-
-    val unifiableWith: List[Clause] = database.clauses
-      .filter(_.functor.identifier == query.question.functor.identifier) // proceed just in function of the name
-      .filter { clause =>
-      clause.head match {
-        case Left(a) => a === query.question match {
-          case Some(_) => true
-          case None => false
-        }
-        case Right(c) => c === query.question match {
-          case Some(_) => true
-          case None => false
-        }
-      }
-    }
-
-    if (unifiableWith.isEmpty) {
-      println(s"no unifiable clause found for $query in ${database.clauses.mkString("\n")}")
-      None
-    }
-    else {
-      println(s"founded clause unifiable with : $unifiableWith")
-      ask(query, unifiableWith)
-    }
-  }
-
-  def ask(query: Query, unifiableWith: List[Clause]): Option[Theta] = unifiableWith match {
-    case Nil => None
-    case c :: cs => unify(query, c) match {
-      case some: Some[Theta] => some
-      case None => ask(query, cs)
-    }
-  }
+  println(ask(q))
 }
