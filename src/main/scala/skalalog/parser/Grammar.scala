c12936b178d7e416486c39431bfb6766805a6822
SnipyJulmy <sylvain.julmy@gmail.com>
2017-11-30 14:47:35 +0100

    Add some utility function and improve some matching

--- src/main/scala/skalalog/parser/Grammar.scala
+++ src/main/scala/skalalog/parser/Grammar.scala
@@ -2,11 +2,38 @@ package skalalog.parser
 
 import skalalog.NoFunctorException
 
-import scala.collection.immutable
+import scala.annotation.tailrec
 
 /* Grammar representation of SkalaLog */
 
 sealed trait Term {
+
+  // return the list of all the variables contained in a Term
+  lazy val variables: List[Variable] = {
+
+    def inner(acc: Set[Variable]): Set[Variable] = this match {
+      case _: Atom => acc
+      case variable: Variable => acc + variable
+      case CompoundTerm(_, args) =>
+        args.map(_.variables).foldLeft(Set[Variable]())((elt, acc) => elt ++ acc)
+      case _: Number => acc
+      case pl: PrologList => pl match {
+        case EmptyList => acc
+        case TermList(elements, end) =>
+          val vars = elements.map(_.variables).foldLeft(Set[Variable]())((elt, acc) => elt ++ acc)
+          end match {
+            case Left(prologList) => vars ++ prologList.variables
+            case Right(variable) => vars + variable
+          }
+
+      }
+      case PrologString(value) => acc
+      case PrologSequence(left, right) => acc ++ left.variables ++ right.variables
+    }
+
+    inner(Set[Variable]()).toList
+  }
+
   def functor: Atom = this match {
     case atom: Atom => atom
     case Variable(identifier) => throw new NoFunctorException(s"no functor for variable $identifier")
@@ -21,6 +48,37 @@ sealed trait Term {
     case _: Atom => 0
     case _: Variable => 0
     case CompoundTerm(_, args) => args.length
+    case _: Number => 0
+    case prologList: PrologList => prologList match {
+      case EmptyList => 0
+      case TermList(elements, end) => end match {
+        case Left(pList) => elements.length + pList.arity
+        case Right(_) => elements.length
+      }
+    }
+    case PrologString(value) => value.length
+    case PrologSequence(_, right) => 1 + right.arity
+  }
+
+  override def toString: String = this match {
+    case Atom(identifier) => identifier
+    case Variable(identifier) => identifier
+    case CompoundTerm(atom, args) => s"$atom(${args.map(_.toString).mkString(",")})"
+    case n: Number => n match {
+      case IntNumber(value) => s"$value"
+      case DoubleNumber(value) => s"$value"
+    }
+    case l: PrologList => l match {
+      case EmptyList => s"[]"
+      case TermList(elements, end) => s"[${elements.map(_.toString).mkString(",")}|${
+        end match {
+          case Left(prologList) => prologList.toString
+          case Right(variable) => variable.toString
+        }
+      }]"
+    }
+    case PrologString(value) => value
+    case PrologSequence(left, right) => s"','($left,$right)"
   }
 }
 
@@ -72,6 +130,11 @@ sealed trait Clause {
   val functor: Atom = head match {
     case Left(value) => value
     case Right(value) => value.atom
+  }
+
+  val headTerm: Term = head match {
+    case Left(atom) => atom
+    case Right(compoundTerm) => compoundTerm
   }
 }
 
